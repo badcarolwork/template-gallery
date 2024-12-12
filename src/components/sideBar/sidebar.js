240d68c9380dfb29cd8371ce6a301a46d669471e
badcarolwork <badcarol.work@gmail.com>
2024-12-12 19:12:56 +0800

    hide interactive video page

--- src/components/sideBar/sidebar.js
+++ src/components/sideBar/sidebar.js
@@ -65,10 +65,10 @@ const Siderbar = () => {
             廣告特效 <br />
             PM AD Filter
           </CustomLink>
-          <CustomLink to="/instream">
+          {/* <CustomLink to="/instream">
             互動影音廣告 <br />
             Interactive In-stream Video
-          </CustomLink>
+          </CustomLink> */}
         </ul>
         <footer>
           <span className="fw-light">© Performics Taiwan | Privacy Policy</span>
